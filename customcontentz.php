4d0e14aec4dc70a26e75f86e2463773235fca7d4
zapalm <zapalm@ya.ru>
2016-11-01 04:43:34 +1000

    [+] added option: prepend a category description to a custom text of the category

--- customcontentz.php
+++ customcontentz.php
@@ -50,6 +50,7 @@ class CustomContentz extends Module
 
         if (Tools::isSubmit('submit_save')) {
             $res  = Configuration::updateValue('CUSTOMCONTENTZ_CAT_FOOTER_TEXT', Tools::getValue('CUSTOMCONTENTZ_CAT_FOOTER_TEXT'));
+            $res &= Configuration::updateValue('CUSTOMCONTENTZ_CAT_DESC', (int)Tools::getValue('CUSTOMCONTENTZ_CAT_DESC'));
             $res &= Configuration::updateValue('CUSTOMCONTENTZ_HOME_TEXT', Tools::getValue('CUSTOMCONTENTZ_HOME_TEXT'));
             $res &= Configuration::updateValue('CUSTOMCONTENTZ_TOP_TEXT', Tools::getValue('CUSTOMCONTENTZ_TOP_TEXT'));
 
@@ -60,26 +61,34 @@ class CustomContentz extends Module
         }
 
         $output .= '
-			<fieldset style="width: 400px">
-				<legend><img src="' . _PS_ADMIN_IMG_ . 'cog.gif" />' . $this->l('Settings') . '</legend>
-					<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
-						<label>' . $this->l('Input a text that will be display on the category footer') . ':</label>
-						<div class="margin-form">
-							<textarea cols=27 rows=5 name="CUSTOMCONTENTZ_CAT_FOOTER_TEXT">' . Configuration::get('CUSTOMCONTENTZ_CAT_FOOTER_TEXT') . '</textarea>
-						</div>
-						<label>' . $this->l('Input a text that will be display on the homepage') . ':</label>
-						<div class="margin-form">
-							<textarea cols=27 rows=5 name="CUSTOMCONTENTZ_HOME_TEXT">' . Configuration::get('CUSTOMCONTENTZ_HOME_TEXT') . '</textarea>
-						</div>						
-						<label>' . $this->l('Input a text that will be display on the top of the site') . ':</label>
-						<div class="margin-form">
-							<textarea cols=27 rows=5 name="CUSTOMCONTENTZ_TOP_TEXT">' . Configuration::get('CUSTOMCONTENTZ_TOP_TEXT') . '</textarea>
-						</div>							
-						<center><input type="submit" name="submit_save" value="' . $this->l('Save') . '" class="button" /></center>
-					</form>
-			</fieldset>
-			<br class="clear">
-		';
+            <fieldset style="width: 400px">
+                <legend><img src="' . _PS_ADMIN_IMG_ . 'cog.gif" />' . $this->l('Settings') . '</legend>
+                    <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
+                        <label>' . $this->l('Input a text that will be display on the category footer') . ':</label>
+                        <div class="margin-form">
+                            <textarea cols=27 rows=5 name="CUSTOMCONTENTZ_CAT_FOOTER_TEXT">' . Configuration::get('CUSTOMCONTENTZ_CAT_FOOTER_TEXT') . '</textarea>
+                        </div>
+
+                        <label>' . $this->l('Prepend a category description to the text.') . '</label>
+                        <div class="margin-form">
+                            <input name="CUSTOMCONTENTZ_CAT_DESC" type="checkbox" value="1"' . (Configuration::get('CUSTOMCONTENTZ_CAT_DESC') ? 'checked="checked"' : '') . '>
+                        </div>
+
+                        <label>' . $this->l('Input a text that will be display on the homepage') . ':</label>
+                        <div class="margin-form">
+                            <textarea cols=27 rows=5 name="CUSTOMCONTENTZ_HOME_TEXT">' . Configuration::get('CUSTOMCONTENTZ_HOME_TEXT') . '</textarea>
+                        </div>
+
+                        <label>' . $this->l('Input a text that will be display on the top of the site') . ':</label>
+                        <div class="margin-form">
+                            <textarea cols=27 rows=5 name="CUSTOMCONTENTZ_TOP_TEXT">' . Configuration::get('CUSTOMCONTENTZ_TOP_TEXT') . '</textarea>
+                        </div>
+
+                        <center><input type="submit" name="submit_save" value="' . $this->l('Save') . '" class="button" /></center>
+                    </form>
+            </fieldset>
+            <br class="clear">
+        ';
 
         return $output;
     }
@@ -115,13 +124,14 @@ class CustomContentz extends Module
 
         // display text on a category page only
         $categoryText = (string)Configuration::get('CUSTOMCONTENTZ_CAT_FOOTER_TEXT');
-        if ($categoryText === '' || (int)Tools::getValue('id_category') === 0) {
+        if ($categoryText === '' || (int)Tools::getValue('id_category') <= 0) {
             return '';
         }
 
-        // TODO: add option - show category description
-        //$category = new Category(Tools::getValue('id_category'), $this->context->language->id);
-        //$categoryDescription = strip_tags($category->description);
+        if (Configuration::get('CUSTOMCONTENTZ_CAT_DESC')) {
+            $category = new Category(Tools::getValue('id_category'), $this->context->language->id);
+            $categoryText = strip_tags($category->description) . ' ' . $categoryText;
+        }
 
         $smarty->assign(array(
             'place'                          => 'footer',
--- ru.php
+++ ru.php
@@ -8,6 +8,7 @@ $_MODULE['<{customcontentz}prestashop>customcontentz_c888438d14855d7d96a2724ee9c
 $_MODULE['<{customcontentz}prestashop>customcontentz_1e103a622ed29a413eadbb2839927386'] = 'Не все настойки сохранены';
 $_MODULE['<{customcontentz}prestashop>customcontentz_f4f70727dc34561dfde1a3c529b6205c'] = 'Настройки';
 $_MODULE['<{customcontentz}prestashop>customcontentz_1d44084b203704157fdffe4c66890aaa'] = 'Напишите текст, который должен показываться в футере категорий';
+$_MODULE['<{customcontentz}prestashop>customcontentz_d6b9f379a793ecd49df0fe4bae8eb091'] = 'Приставить к началу текста описание категории.';
 $_MODULE['<{customcontentz}prestashop>customcontentz_a491842121eb8aedce72569a9f42ebf7'] = 'Напишите текст, который должен показываться на домашней странице';
 $_MODULE['<{customcontentz}prestashop>customcontentz_f124b0e95f2e1aa5a1b28a0c9b9abc4d'] = 'Напишите текст, который должен показываться в топе сайта';
 $_MODULE['<{customcontentz}prestashop>customcontentz_c9cc8cce247e49bae79f15173ce97354'] = 'Сохранить';
